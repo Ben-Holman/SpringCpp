372bbc83892db87256094a06dc97e8deb6b3b062
Ben-Holman <generalmoses99@gmail.com>
2022-02-18 11:25:20 -0500

    changes to 5.5 and 5.6

--- Holman_ex56/Holman_ex56/Holman_ex56.cpp
+++ Holman_ex56/Holman_ex56/Holman_ex56.cpp
@@ -7,9 +7,12 @@ using namespace std;
 // This variable is defined globally, i.e. it is known to all functions in this program as PI 
 // To declare a global constant you must write it outside the main() function
 const double PI = 3.14159;
+
 //Now we declare any programmer defined function
 double cross_area(double r); // Function prototype for function cross_area 
 double side_area(double r, double h); // Function prototype for function Side_area 
+double total_calc(double r, double h); // function prototype for calculating the total area. 
+
 // Start defining the main function
 int main(void)
 {
@@ -23,6 +26,7 @@ int main(void)
 	cout << "in the second one I will convert h \n";
 	cout << "The cross section area of the cylinder is " << cross_area(r) << " inch-sqr" << endl;
 	cout << "The side area of the cylinder is " << side_area(r, h) << " inch-sqr \n\n";
+	cout << "The total area of the cylinder is " << total_calc(r, h) << "inch-sqr\n\n"; 
 	return 0;
 }
 // Definition of all programmer defined functions
@@ -36,13 +40,24 @@ double side_area(double r, double h)
 {
 	double area; //variable local to side_area function
 	h = h * 0.3937; // converting h to inch
+	r = r * 0.3937; // converting r to inches just like in cross_area. 
 	area = 2 * PI * r * h;
 	return area;
 }
 
-double total_calc() { // calcualates the total area
-
+double total_calc(double r, double h) {
+	double total; 
+	total = cross_area(r) + side_area(r,h);
+	return total; 
 }
-// include a function called total_area that computes the total surface area for the cylinder. 
-// side area + 2x the cross-section 
-// the area must be 27.2689
+
+/*The correct answer should be:
+ The cross-section area of the cylinder is 3.89556 inch-sq -- this is correct
+ The side area of the cylinder is 19.4778 inch-sqr -- this is not correct -- why.
+ for side_area, changed r * h to pow(r,h)g ave me a very high number. I figured that would be the case, but wanted to test it anyway.
+ Then I removed the 2 on line 41. Did not work. Got closer, but did not work.
+ needed to add the radius conversion.
+
+
+ The test case passes.
+ */
