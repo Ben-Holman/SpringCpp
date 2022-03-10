631ce20230de30d2ad144d97344d91b2a88783d7
Ben Holman <58403382+GeneralMoses@users.noreply.github.com>
2022-03-09 22:23:03 -0500

    the challenge program for week 7

---
+++ ChallengeWeek7/ChallengeWeek7/challenge.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std; 
+int triangle(); 
+int upsideDownTirangle(); 
+int square(); 
+int rectangle(); 
+
+int main() {
+
+	return 0; 
+}
+
+int triangle() {
+
+}
+int upsideDownTirangle() {
+
+}
+int square() {
+
+}
+int rectangle() {
+
+}
+
