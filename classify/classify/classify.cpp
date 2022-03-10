41a330a2dff40927fc8f84a3c2400ab6508dfa26
Ben Holman <58403382+GeneralMoses@users.noreply.github.com>
2022-03-09 22:18:23 -0500

    problem of the week Lab 7

---
+++ classify/classify/classify.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std; 
+
+void initialize(); 
+void getNumber(); 
+void classifyNumber(); 
+void printResults(); 
+
+int main() {
+	initialize(); 
+	getNumber(); 
+	classifyNumber(); 
+	printResults(); 
+	return 0; 
+}
+void initialize() {
+	cout << "Please enter 20 integers:" << endl; 
+}
+
+void getNumber() {
+	int integers = 0; 
+
+}
+
+void classifyNumber() {
+
+}
+
+void printResults() {
+
+}
