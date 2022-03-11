ef239b3e0585e1ba4e0c86aac5381cfaa7157e30
Ben-Holman <generalmoses99@gmail.com>
2022-03-11 11:19:24 -0500

    almost done on both

--- ChallengeWeek7/ChallengeWeek7/challenge.cpp
+++ ChallengeWeek7/ChallengeWeek7/challenge.cpp
@@ -10,6 +10,8 @@ int input(); // grabs the input of the size from the user
 char character_input(); // grabs the user input for the character they want to use
 void ScreenDisplay(); // displays the intial message for the user to the screen 
 void options(int size, char character); // the real workhorse. Takes the size and chracter input from the other two functions and gives theuser the option to pick what shape they want to draw. 
+void sideWaysTriangle(int size, char character);
+void salutations(); 
 
 										//-----------------------------------MAIN-------------------------------------------
 
@@ -18,6 +20,8 @@ int main() {
 	int size = input();
 	char character = character_input();
 	options(size, character); 
+	salutations();  
+
 	return 0; 
 }
 										//--------------------------------FUNCTIONS------------------------------------------
@@ -42,20 +46,44 @@ void upsideDownTriangle(int size, char character) {
 	character;
 	space = 1;
 	//cout << endl; 
-	for (int i = 1; i <= size - 1; i++)
+	for (int i = 1; i <= 2 * (size - i) - 1; i++)
+	{
+		for (int j = 1; j <= space; j++) 
+			cout << character;
+			space++;
+		for (int k = 1; k <=2 *(size - 1); k++) 
+			cout << " ";
+
+		cout << "\n";
+	} // A different upside down triangle, look at example. 
+	// I reversed the original triangle loop from the diamond function. 
+}
+
+void sideWaysTriangle(int size, char character) {
+	upsideDownTriangle(size, character); 
+	int space;
+	character;
+	space = 1;
+	//cout << endl; 
+	for (int i = 1; i <= size - 1 - 1; i++)
 	{
 		for (int j = 1; j <= space; j++)
-			cout << " ";
-		space++;
-		for (int k = 1; k <= 2 * (size - i) - 1; k++)
 			cout << character;
+		space++;
+		for (int k = 1; k <= 2 * (size - i) -1; k++)
+			cout << " ";
+
 		cout << "\n";
-	} // in class we used 2 * rowNumber + 1, I used 2 * i - 1 because my variable starts at 1. 
+	} // A different upside down triangle, look at example. 
+	// I reversed the original triangle loop from the diamond function. 
+	// I have half of the sideways triangle, just need the bottom half. 
+
+	cout << endl;
 }
 
 void square(int size, char character) {
-	int rows;
-	for (rows = 0; rows != size; rows++) {
+	for (int rows = 0; rows != size; rows++) {
+		//cout << endl; 
 		for (int columns = 0; columns != size; columns++) {
 			if ((rows == 0) || (rows = size - 1) || (columns == 0) || (columns == size - 1)) {
 				cout << character << " ";
@@ -72,13 +100,13 @@ void rectangle(int size, char character) {
 	int rows;
 	for (rows = 0; rows != size; rows++) {
 		for (int columns = 0; columns != size; columns++) {
-			if ((rows == 0) || (rows = size - 1) || (columns == 0) || (columns == size - 1)) {
-				cout << character << " " << endl;
+			if ((rows == 0) || (rows = size - 1) || (columns == 0) || (columns == size - 1)) { 
+				cout << character << " "; 
 			}
 			else {
 				cout << "  ";
 			}
-
+			
 		}
 		cout << "\n\n";
 	}
@@ -91,6 +119,7 @@ int input() {
 	cin >> size;
 	return size;
 }
+
 char character_input() {
 	char character;
 	cout << "What character would you like to print? Star or Ampersand? (*/&)\n";
@@ -108,7 +137,12 @@ char character_input() {
 }
 void options(int size, char character) {
 	int options; 
-	cout << "Choose what Shape you would like to draw\n Enter the number that cooresponds to the shape in the list:\n [1] Regular Triangle\n [2] Upside Down Triangle\n [3] Sideways Triangle\n [4] Square\n [5] Rectangle\n\n"; 
+	cout << "Choose what Shape you would like to draw\n Enter the number that cooresponds to the shape in the list:\n "
+		<< "[1] Regular Triangle\n "
+		<< "[2] Upside Down Triangle\n "
+		<< "[3] Sideways Triangle\n "
+		<< "[4] Square\n "
+		<< "[5] Rectangle\n";
 	cin >> options; 
 	switch (options) {
 	case 1:
@@ -120,8 +154,8 @@ void options(int size, char character) {
 		return upsideDownTriangle(size, character);
 		break;
 	case 3:
-		cout << "You Chose Sideways Triangle\n"; 
-		//return options; 
+		cout << "You Chose Sideways Triangle\n";
+		return sideWaysTriangle(size, character);
 		break;
 	case 4:
 		cout << "You Chose Square\n"; 
@@ -137,6 +171,13 @@ void options(int size, char character) {
 
 void ScreenDisplay() {
 	cout << "Welcome to the Shape Drawing Program!\n";
+}
+
+void salutations() {
+	cout << "thank you for using our app specifically designed for you!\n"; 
+	cout << "We Hope you enjoyed it!\n"; 
+	cout << "Please continue looking for our future products\n"; 
+	cout << "Goodbye!!!\n\n"; 
 }
 /*
 * I copied most of the code from Pascal's Triangle program. I used the diamond loops for the upside down triangle. 
--- classify/classify/classify.cpp
+++ classify/classify/classify.cpp
@@ -5,31 +5,55 @@ using namespace std;
 
 void initialize(); 
 void getNumber(); 
-void classifyNumber(); 
+void classifyNumber(int& numbers, int& evens, int& odds, int& zeroes);
 void printResults(); 
 
 int main() {
-	initialize(); 
+	initialize();
+	for (int i = 1; i < 20; i++) {
+		getNumber();
+	}
 	getNumber(); 
-	classifyNumber(); 
+	//classifyNumber(); 
 	printResults(); 
 	return 0; 
 }
 void initialize() {
+	int zeros = 0; 
+	int evens = 0; 
+	int odds = 0; 
 	cout << "Please enter 20 integers:" << endl; 
 }
 
 void getNumber() {
-	int integers = 0; 
+	int integers; 
+	cin >> integers; 
 
 }
 
-void classifyNumber() {
+void classifyNumber(int& numbers, int& evens, int& odds, int& zeroes) {
+	initialize();
+	int evensCount = evens; 
+	int oddsCount = odds; 
+	int zeroCount = zeroes; 
+	switch (numbers % 2) {
+	case 0:
+		cout << "even\n"; 
+		evensCount++; 
+		zeroCount++; 
+		cout << "There are " << evensCount << " with " << zeroCount << " zeroes.\n";
+		break; 
+	case 1:
+		cout << "odd\n"; 
+		oddsCount++;  
+		cout << "There are " << oddsCount << "odds"; 
+		break; 
+	}
 
 }
 
 void printResults() {
-
+	//classifyNumber(); 
 }
 
 // see if you can't store the integers in a text file and then sort it from the file. in classify()
