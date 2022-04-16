4cac8e29dfb4a0aa436c164383761660ad0cee51
Ben Holman <58403382+GeneralMoses@users.noreply.github.com>
2022-04-16 00:42:15 -0400

    hopefully final versions

--- Holman_ex101/Holman_ex101/Holman_ex101.cpp
+++ Holman_ex101/Holman_ex101/Holman_ex101.cpp
@@ -2,43 +2,118 @@
 #include<iostream> 
 #include<cstring> 
 using namespace std;
+/*
+void swap(char& v1, char& v2);
+string reverse(const string& s);
+string removePunct(const string& s, const string& punct);
+string makeLower(const string& s);
+bool isPal(char str[]);
+*/
+bool isPal(char str[]);
 int main()
 {
     char s1[8], s2[8];
-    int length1, length2;
+    int length1, length2, length3, j;
+    j = 0;
     int count = 0;
     // Initialize the two strings 
-    strcpy(s1, "Black");
-    strcpy(s2, "Black");
-    while (count < 2)
+    strcpy_s(s1, "Black");
+    strcpy_s(s2, "Black");
+    //got an error for the regular strcpy so changed it to strcpy_s 
+
+
+
+    // After the first time ask the user to enter them 
+
+    cout << "Now I let you enter two strings \n";
+    cout << "Enter the first string, then the second \n";
+    // Another way to initialize the two strings 
+    cin >> s1 >> s2;
+
+    // Find their lengths 
+    length1 = strlen(s1);
+    length2 = strlen(s2);
+    // Compare their length 
+    if (length1 == length2)
     {
-        if (count > 0)
-            // After the first time ask the user to enter them 
-        {
-            cout << "Now I let you enter two strings \n";
-            cout << "Enter the first string, then the second \n";
-            // Another way to initialize the two strings 
-            cin >> s1 >> s2;
-        }
-        // Find their lengths 
-        length1 = strlen(s1);
-        length2 = strlen(s2);
-        // Compare their length 
-        if (length1 == length2)
-        {
-            cout << "The two strings are the same length, are they the same? \n";
-        }
-        // See if they are the same 
-        if (!strcmp(s1, s2))
-        {
-            cout << "The two strings: ";
-            cout << s1 << " and " << s2 << " are the same \n";
-        }
-        else
-        {
-            cout << "The two strings: ";
-            cout << s1 << " and " << s2 << " are NOT the same \n";
-        }
-        count++;
+        cout << "The two strings are the same length, are they the same? \n";
     }
-    return 0;
+    // See if they are the same 
+    if (!strcmp(s1, s2))
+    {
+        cout << "The two strings: ";
+        cout << s1 << " and " << s2 << " are the same \n";
+        cout << "\n Let's combine the strings and reverse the String and see if the string is a palindrome:\n";
+        strncat_s(s1, s2, 50);
+        length1 = strlen(s1);
+        j = length1 - 1;
+        char temp;
+        for (int i = 0; i <= (length1 - 1) / 2; i++) {
+            temp = s1[i];
+            s1[i] = s1[j];
+            s1[j] = temp;
+            j--;
+        }
+        cout << "The Reversed String is:\n";
+        cout << s1;
+    }
+    else
+    {
+        cout << "The two strings: ";
+        cout << s1 << " and " << s2 << " are NOT the same \n";
+    }
+    /*
+    cout << "\n Let's reverse the String and see if the string is a palindrome:\n";
+    strncat_s(s1, s2, 20);
+    length3 = strlen(s1);
+    j = length3 - 1;
+    char temp;
+    for (int i = 0; i <= (length3-1)/2; i++) {
+        temp = s1[i];
+        s1[i] = s1[j];
+        s1[j] = temp;
+        j--;
+    }
+    cout << "The Reversed String is:\n";
+    cout << s1;
+    */
+
+
+    /*
+    char string1[10];
+    char string2[10];
+    cout << "Enter 2 String inputs\n";
+    cin >> string1 >> string2;
+    strncat_s(string1, string2, 5);
+    */
+
+    char cString1[50];
+    char cString2[50];
+    //char combinedString[150];
+    cout << "Enter a candidate for palindrome test\n";
+    cin >> cString1 >> cString2;
+    //cin >> cString2;
+    strcat_s(cString1, cString2);
+    //cout << cString1; // test print of the string 
+    if (isPal(cString1))
+        cout << "\"" << cString1 << "\" is a palindrome.";
+    else
+        cout << "\"" << cString1 << "\" is not a palindrome.";
+    cout << endl;
+    return  0;
+}
+
+bool isPal(char str[] ) {
+    int mid = strlen(str) / 2;
+    int length = strlen(str); 
+    for (int i = 0; i < length; i++) {
+        if (str[i] != str[length - i - 1]) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
+
+
--- Holman_ex102/Holman_ex102/Holman_ex102.cpp
+++ Holman_ex102/Holman_ex102/Holman_ex102.cpp
@@ -20,7 +20,28 @@ int main()
     temp1 = newphrase.substr(0, 18);
     cout << temp1 << endl;
     // separates the second sentence 
-    temp2 = newphrase.substr(18, 10);
+    temp2 = newphrase.substr(18, 12);
     cout << temp2 << endl;
+
+    // ==== LAB START ====
+
+    string str;
+    char replace = 'G';
+    char newChar = 'g';
+    getline(cin,str); 
+    cout << str; 
+    int length = str.length();
+    for (int i = 0; i < length; i++){
+        if (str[i] == replace) {
+            str[i] = newChar; 
+        }
+    }
+    cout << str; 
+    //string1 = str; 
+   // size_t found = str.find(find);
+    //if (found != string::npos)
+        //str = tolower(find);
+       // cout << "First occurrence is " << found << endl;
+    //cout << string1 + str;
     return 0;
 }
